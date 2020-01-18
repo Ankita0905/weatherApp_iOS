06ae70edfaaadcc5f349ef52376effe969417e51
Ankita Jain <ankita.jain12342@gmail.com>
2020-01-17 20:43:48 -0500

    init

--- weatherApp_iOS/Controller/ForecastTableViewCell.swift
+++ weatherApp_iOS/Controller/ForecastTableViewCell.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ForecastTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var timeLbl: UILabel!
+    @IBOutlet weak var highTempLbl: UILabel!
+    @IBOutlet weak var lowTempLbl: UILabel!
+    @IBOutlet weak var tempLbl: UILabel!
+    @IBOutlet weak var feelLbl: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
